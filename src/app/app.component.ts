166df7b8b93444e8d7fe0758d50c537fb38e3b90
Leifer Mendez <leifer33@gmail.com>
2021-06-14 20:45:40 +0200

    First

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'angular-comunicacion-componentes';
+  ngOnInit(): void {
+  }
+
+}
